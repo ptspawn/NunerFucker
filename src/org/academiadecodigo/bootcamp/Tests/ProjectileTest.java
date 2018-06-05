477dba7af02ec55b96f78a614bbaeed35ad3a87d
Tiago Lage <tiagoteixeiralage@gmail.com>
2018-06-05 09:33:26 +0100

    fix issue with bullet shoot width/getX

--- src/org/academiadecodigo/bootcamp/Tests/ProjectileTest.java
+++ src/org/academiadecodigo/bootcamp/Tests/ProjectileTest.java
@@ -1,7 +1,6 @@
 package org.academiadecodigo.bootcamp.Tests;
 
 import org.academiadecodigo.bootcamp.GameEngine.InputManager;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.academiadecodigo.bootcamp.Projectiles.Bullet;
 import org.academiadecodigo.bootcamp.Projectiles.Projectile;
 import org.academiadecodigo.bootcamp.Projectiles.ProjectileFactory;
