05018fb348cbd965a220b4fc42041caf181d9d51
RitaCasal <rita.casal@outlook.com>
2018-06-09 18:10:42 +0100

    new sounds

--- src/org/academiadecodigo/bootcamp/Sound/SoundType.java
+++ src/org/academiadecodigo/bootcamp/Sound/SoundType.java
@@ -2,6 +2,14 @@ package org.academiadecodigo.bootcamp.Sound;
 
 public enum SoundType {
     BACKGROUND,
+    BACKGROUND2,
+    BACKGROUND3,
+    BACKGROUND4,
+    BOSS,
+    ELEVATOR,
+    GAMEOVER,
+    MENU,
+    ROCKET,
     GUN,
     PUNCH;
 
@@ -10,6 +18,29 @@ public enum SoundType {
             case BACKGROUND:
                 Sound.playLoop("Resources/Sounds/background.wav");
                 break;
+            case BACKGROUND2:
+                Sound.playLoop("Resources/Sounds/background2.wav");
+                break;
+            case BACKGROUND3:
+                Sound.playLoop("Resources/Sounds/background3.wav");
+                break;
+            case BACKGROUND4:
+                Sound.playLoop("Resources/Sounds/background4.wav");
+                break;
+            case BOSS:
+                Sound.playLoop("Resources/Sounds/boss.wav");
+                break;
+            case ELEVATOR:
+                Sound.playLoop("Resources/Sounds/elevator.wav");
+                break;
+            case GAMEOVER:
+                Sound.playLoop("Resources/Sounds/gameOver.wav");
+                break;
+            case MENU:
+                Sound.playLoop("Resources/Sounds/menu.wav");
+                break;
+            case ROCKET:
+                Sound.playLoop("Resources/Sounds/rocket.wav");
             case GUN:
                 Sound.playOnce("Resources/Sounds/gun.wav");
                 break;
