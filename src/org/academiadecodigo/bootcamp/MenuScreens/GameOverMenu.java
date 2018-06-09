0162d7e94bd4598394732793fbb7927a02ec5c9c
Tiago Lage <tiagoteixeiralage@gmail.com>
2018-06-10 00:37:07 +0100

    working on gameover menu

--- src/org/academiadecodigo/bootcamp/MenuScreens/GameOverMenu.java
+++ src/org/academiadecodigo/bootcamp/MenuScreens/GameOverMenu.java
@@ -17,7 +17,11 @@ public class GameOverMenu {
     private Picture background;
     private Picture gameOver;
     private Picture highScoreBoard;
-
+    private Picture quitButton;
+    private Picture youSuck;
+    private Picture startButton;
+    private Picture startPopUp;
+    boolean onStartButton;
 
     public GameOverMenu() {
 
@@ -37,10 +41,75 @@ public class GameOverMenu {
         highScoreBoard = new Picture(1080, SCREENDIMENTIONS[1] - 961, "Bgs/highscore.png");
         highScoreBoard.draw();
 
+        startButton = new Picture(1100, 410, "Bgs/video-play.png");
+
+        startButton.draw();
+        quitButton = new Picture(1805, 20, "Bgs/Door-Closed.png");
+
+        quitButton.draw();
+        youSuck = new Picture(1808, 20, "Bgs/middlefinger.png");
+
+        startPopUp = new Picture(1100, 410, "Bgs/2c_go.png");
+
         highScores();
 
         Canvas.getInstance().repaint();
 
+    }
+
+
+    public int checkButtons(double[] mousePos) {
+
+        int result = 0;
+
+        if (checkStartButton(mousePos)) {
+            result = 1;
+        }
+        if (quiting(mousePos)) {
+            result = 2;
+        }
+
+
+        return result;
+
+    }
+
+    private boolean checkStartButton(double[] mousePos) {
+
+        if (mousePos[0] > startButton.getX()
+                && mousePos[0] < startButton.getX() + startButton.getWidth()
+                && mousePos[1] > startButton.getY()
+                && mousePos[1] < startButton.getY() + startButton.getHeight()) {
+
+            startButton.delete();
+            startPopUp.draw();
+            onStartButton = true;
+            return true;
+
+        } else {
+            startPopUp.delete();
+            onStartButton = false;
+            return false;
+
+        }
+    }
+
+    private boolean quiting(double[] mousePos) {
+
+        if (mousePos[0] > quitButton.getX()
+                && mousePos[0] < quitButton.getX() + quitButton.getWidth()
+                && mousePos[1] > quitButton.getY()
+                && mousePos[1] < quitButton.getY() + quitButton.getHeight()) {
+
+            quitButton.delete();
+            youSuck.draw();
+            return true;
+
+        } else {
+            youSuck.delete();
+            quitButton.draw();
+            return false;
+        }
     }
 
 
@@ -49,7 +118,7 @@ public class GameOverMenu {
         FileManagement fileManagement = new FileManagement();
         String[] highScores = fileManagement.getHighScores();
 
-        for (int i = 0; i <highScores.length; i++) {
+        for (int i = 0; i < highScores.length; i++) {
             if (highScores[i] != null) {
                 Text text = new Text(1300, 480 + i * 30, highScores[i]);
                 text.draw();
--- src/org/academiadecodigo/bootcamp/Tests/GameOverTest.java
+++ src/org/academiadecodigo/bootcamp/Tests/GameOverTest.java
@@ -1,6 +1,8 @@
 package org.academiadecodigo.bootcamp.Tests;
 
+import org.academiadecodigo.bootcamp.GameEngine.InputManager;
 import org.academiadecodigo.bootcamp.MenuScreens.GameOverMenu;
+import org.academiadecodigo.notsosimplegraphics.graphics.Canvas;
 
 /**
  * Created by codecadet on 09/06/2018.
@@ -8,8 +10,37 @@ import org.academiadecodigo.bootcamp.MenuScreens.GameOverMenu;
 public class GameOverTest {
 
     public static void main(String[] args) {
-
+        InputManager input = new InputManager();
         GameOverMenu gameOverMenu = new GameOverMenu();
+        double[] mousePosition;
+
+        while (true) {
+
+            mousePosition = input.getMousePos();
+            int result = 0;
+
+            if ((result = gameOverMenu.checkButtons(mousePosition)) != 0 && input.isFiring()) {
+
+                switch (result) {
+
+                    case 1:
+                //        start();
+                    case 2:
+                        System.exit(0);
+
+                }
+
+            }
+
+            Canvas.getInstance().repaint();
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                System.out.println("Ups");
+            }
+
+        }
 
     }
 }
